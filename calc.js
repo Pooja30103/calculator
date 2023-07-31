53b2548cabeea7ed46e44650f37292372a0adf8b
Pooja30103 <138848860+Pooja30103@users.noreply.github.com>
2023-07-31 16:35:54 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,12 @@
+function add(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a+b;
+    document.getElementById("button1").innerHTML="sum of number:"+c;
+}
+function sub(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a-b;
+    document.getElementById("button2").innerHTML="Difference of number:"+c;
+}
